d9b85300011935c7f753a06c5fcc391eba5c41ef
Sandhil Doolwala <doolwala.sandhil@gmail.com>
2023-12-08 19:35:18 +0530

    img-error2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["firebasestorage.googleapis.com", "image.made-in-china.com"],
-        formats: ["image/webp"],
-    }
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
