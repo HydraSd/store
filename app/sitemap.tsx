91ee9f102eea62a2cd08619d4fda97d07055147e
Sandhil Doolwala <doolwala.sandhil@gmail.com>
2024-01-10 11:14:14 +0530

    robot.txt fixed

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'http://localhost:3000/',
+      url: 'https://ssdauto.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
