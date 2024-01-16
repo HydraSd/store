1efc61f22c4caaabb42e7b14309c9b3a9170c661
Sandhil Doolwala <doolwala.sandhil@gmail.com>
2024-01-17 00:01:51 +0530

    robot.txt updated

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ssdauto.vercel.app',
+      url: 'https://doolmotors.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
