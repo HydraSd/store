77215510c2948c5663480b6443a88e22737ba5d6
Sandhil Doolwala <doolwala.sandhil@gmail.com>
2024-01-25 21:21:00 +0530

    blog loading added

---
+++ app/(root)/blog/error.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+type Props = {}
+
+function error({}: Props) {
+  return (
+    <center className='mt-5'>No result</center>
+  )
+}
+
+export default error
