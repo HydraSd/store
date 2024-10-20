73b2bad8bd841499e65d043faf311a0dc7d88bbb
Sandhil Doolwala <doolwala.sandhil@gmail.com>
2024-10-20 18:44:41 +0530

    added social meta tags

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,13 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import HeaderTop from "@/components/Header/HeaderTop";
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header/Header";
 import ToastPorvider from "@/providers/toast-provider";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // import Footer from '@/components/Footer'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,12 +24,43 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <script src="//code.tidio.co/of3prjoosirww7kz3w8cufwx73px5mkq.js" async></script>
+      <script
+        src="//code.tidio.co/of3prjoosirww7kz3w8cufwx73px5mkq.js"
+        async
+      ></script>
       <head>
         <meta
           name="google-site-verification"
           content="v9c2CEKmgiTTg8-_C_9DIsWD0_VFZCFM_7Fkh48uaTI"
         />
+
+        {/* Facebook */}
+        <meta property="og:url" content="https://doolmotors.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="DOOL Motors: Affordable Spare Parts in Sri Lanka"
+        />
+        <meta
+          property="og:description"
+          content="DOOL Motors: Your go-to for genuine, affordable vehicle parts in Sri Lanka. Explore a wide range with seamless online shopping and quick delivery."
+        />
+        <meta property="og:image" content="/logo/logodool.png" />
+
+        {/* Tiwitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="doolmotors.com" />
+        <meta property="twitter:url" content="https://doolmotors.com" />
+        <meta
+          name="twitter:title"
+          content="DOOL Motors: Affordable Spare Parts in Sri Lanka"
+        />
+        <meta
+          name="twitter:description"
+          content="DOOL Motors: Your go-to for genuine, affordable vehicle parts in Sri Lanka. Explore a wide range with seamless online shopping and quick delivery."
+        />
+        <meta name="twitter:image" content="/logo/logodool.png"></meta>
+
         <link rel="icon" href="/logo/logodool.png" sizes="any" />
       </head>
       <body className={inter.className}>
@@ -41,7 +72,7 @@ export default function RootLayout({
         <SpeedInsights />
         {/* <Footer /> */}
       </body>
-      <GoogleAnalytics gaId='G-VTZYJGPBPM' />
+      <GoogleAnalytics gaId="G-VTZYJGPBPM" />
     </html>
   );
 }
